d2c701e68f78b0afae1bea2879a4f008895e0ef6
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-07-13 17:05:49 +0800

    Revert "20230713A"
    This reverts commit f151cc558e391df574a55389c1624ece0628708b.

--- lib/array.ts
+++ lib/array.ts
@@ -1,4 +1,4 @@
-import { equal } from "../deps.ts";
+import { equal } from "https://deno.land/std@0.193.0/testing/asserts.ts";
 const arrayIndexRegExp = /^(?:0|[1-9]\d*)$/u;
 /**
  * @function isArrayStrict

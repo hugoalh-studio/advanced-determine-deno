3448f54af13cc0ea93afa3f1dd465a96d54a8399
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-07-29 15:46:10 +0800

    20230729B

--- native.ts
+++ native.ts
@@ -1,8 +1,8 @@
-export { isArrayStrict, isArrayUnique, isArrayUniqueReference } from "./lib/array.ts";
-export { isBigIntegerEven, isBigIntegerIntegralNumericType, isBigIntegerNegative, isBigIntegerOdd, isBigIntegerPositive, isBigIntegerPrime, isBigIntegerSafe, isBigIntEven, isBigIntIntegralNumericType, isBigIntNegative, isBigIntOdd, isBigIntPositive, isBigIntPrime, isBigIntSafe } from "./lib/bigint.ts";
-export { isAsyncFunction, isAsyncGeneratorFunction, isAsynchronousFunction, isAsynchronousGeneratorFunction, isSyncFunction, isSyncGeneratorFunction, isSynchronousFunction, isSynchronousGeneratorFunction } from "./lib/function.ts";
-export { isAsyncGenerator, isAsynchronousGenerator, isSyncGenerator, isSynchronousGenerator } from "./lib/generator.ts";
-export { type IntegralNumericTypeEnumKeysType } from "./lib/internal/enum.ts";
-export { isNumberEven, isNumberFloat, isNumberIntegralNumericType, isNumberNegative, isNumberOdd, isNumberPositive, isNumberPrime, isNumberSafe } from "./lib/number.ts";
-export { isObjectPlain } from "./lib/object.ts";
-export { isStringASCII, isStringLowerCase, isStringMultipleLine, isStringSingleLine, isStringUpperCase } from "./lib/string.ts";
+export { isArrayStrict, isArrayUnique, isArrayUniqueReference } from "./array.ts";
+export { isBigIntegerEven, isBigIntegerIntegralNumericType, isBigIntegerNegative, isBigIntegerOdd, isBigIntegerPositive, isBigIntegerPrime, isBigIntegerSafe, isBigIntEven, isBigIntIntegralNumericType, isBigIntNegative, isBigIntOdd, isBigIntPositive, isBigIntPrime, isBigIntSafe } from "./bigint.ts";
+export { isAsyncFunction, isAsyncGeneratorFunction, isAsynchronousFunction, isAsynchronousGeneratorFunction, isSyncFunction, isSyncGeneratorFunction, isSynchronousFunction, isSynchronousGeneratorFunction } from "./function.ts";
+export { isAsyncGenerator, isAsynchronousGenerator, isSyncGenerator, isSynchronousGenerator } from "./generator.ts";
+export { type IntegralNumericTypeEnumKeysType } from "./internal/enum.ts";
+export { isNumberEven, isNumberFloat, isNumberIntegralNumericType, isNumberNegative, isNumberOdd, isNumberPositive, isNumberPrime, isNumberSafe } from "./number.ts";
+export { isObjectPlain } from "./object.ts";
+export { isStringASCII, isStringLowerCase, isStringMultipleLine, isStringSingleLine, isStringUpperCase } from "./string.ts";

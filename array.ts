e35346fd37aaad9dd5a2c8d09acdc4a413717101
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-08-25 16:24:47 +0800

    Update dependencies

--- array.ts
+++ array.ts
@@ -1,4 +1,4 @@
-import uniqueArray from "https://deno.land/x/unique_array@v1.0.8/mod.ts";
+import uniqueArray from "https://deno.land/x/unique_array@v1.0.9/mod.ts";
 const arrayIndexRegExp = /^(?:0|[1-9]\d*)$/u;
 /**
  * Determine whether the array is not contain custom defined properties.

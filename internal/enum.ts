1ff269027b4ff9f068533a0fa80e3673fc2e962b
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-09-05 12:36:07 +0800

    Fix resolver

--- internal/enum.ts
+++ internal/enum.ts
@@ -1,27 +1,27 @@
 export type EnumCase<T extends string> = T | Capitalize<T> | Uncapitalize<T>;
 /**
- * @template {unknown} Eo
- * @template {unknown} Es
- * @param {Eo} enumObject
- * @param {Eo | Es} input
+ * @template {unknown} O
+ * @template {unknown} S
+ * @param {object} enumObject
+ * @param {O | S} input
  * @param {string} parameterDescription
- * @returns {Eo}
+ * @returns {O}
  */
-export function resolveEnum<Eo, Es>(enumObject: object, input: Eo | Es, parameterDescription: string): Eo {
-	if (Object.values(enumObject as object).includes(input)) {
-		return input as Eo;
+export function resolveEnum<O, S>(enumObject: object, input: O | S, parameterDescription: string): O {
+	if (Object.values(enumObject).includes(input)) {
+		return input as O;
 	}
 	if (typeof input !== "string") {
 		throw new TypeError(`${parameterDescription.slice(0, 1).toUpperCase()}${parameterDescription.slice(1)} is not a string!`);
 	}
-	for (const key of Object.keys(enumObject as object)) {
+	for (const key of Object.keys(enumObject)) {
 		if (
 			input === key ||
 			input === `${key.slice(0, 1).toLowerCase()}${key.slice(1)}` ||
 			input === `${key.slice(0, 1).toUpperCase()}${key.slice(1)}`
 		) {
 			//@ts-ignore Determine error.
-			return enumObject[key] as Eo;
+			return enumObject[key] as O;
 		}
 	}
 	throw new RangeError(`\`${input}\` is not a valid value for ${parameterDescription.slice(0, 1).toLowerCase()}${parameterDescription.slice(1)}! Only accept these values: "${Array.from(new Set(Object.keys(enumObject as object).flatMap((value: string): string[] => {

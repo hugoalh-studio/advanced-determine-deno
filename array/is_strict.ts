66572cd534d7d34ae123503a94118db3bedce0fb
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-09-07 12:59:26 +0800

    Add example

--- array/is_strict.ts
+++ array/is_strict.ts
@@ -3,6 +3,12 @@ const arrayIndexRegExp = /^(?:0|[1-9]\d*)$/u;
  * Determine whether the array is not contain custom defined properties.
  * @param {unknown[]} item Item that need to determine.
  * @returns {boolean} Determine result.
+ * @example
+ * isArrayStrict(Object.freeze([1, 2, 3, "foo", "bar", "baz"]) as unknown[]);
+ * //=> false
+ * @example
+ * isArrayStrict([1, 2, 3, "foo", "bar", "baz"]);
+ * //=> true
  */
 export function isArrayStrict(item: unknown[]): boolean {
 	const itemPrototype: unknown = Object.getPrototypeOf(item);

2038a88b9f022362cf832c35377af0a1fb21a7ad
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-09-09 12:15:08 +0800

    Update JSDoc

--- is_async_generator.ts
+++ is_async_generator.ts
@@ -1,6 +1,8 @@
 import { types } from "node:util";
 /**
- * Determine whether the item is an asynchronous generator. This only reports back what the JavaScript engine is seeing; In particular, the return value may not match the original source code if a transpilation tool was used.
+ * Determine whether the item is an asynchronous generator.
+ * 
+ * This only reports back what the JavaScript engine is seeing; In particular, the return value may not match the original source code if a transpilation tool was used.
  * @param {unknown} item Item that need to determine.
  * @returns {item is AsyncGenerator<unknown, unknown, unknown>} Determine result.
  */
--- is_sync_generator_function.ts
+++ is_sync_generator_function.ts
@@ -1,6 +1,8 @@
 import { types } from "node:util";
 /**
- * Determine whether the item is a synchronous generator function. This only reports back what the JavaScript engine is seeing; In particular, the return value may not match the original source code if a transpilation tool was used.
+ * Determine whether the item is a synchronous generator function.
+ * 
+ * This only reports back what the JavaScript engine is seeing; In particular, the return value may not match the original source code if a transpilation tool was used.
  * @param {unknown} item Item that need to determine.
  * @returns {item is GeneratorFunction} Determine result.
  */

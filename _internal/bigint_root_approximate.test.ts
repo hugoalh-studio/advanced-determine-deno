6e2df11610f820f308f352b596f49eb7dc35222a
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-10-04 12:26:57 +0800

    Update test

---
+++ _internal/bigint_root_approximate.test.ts
@@ -0,0 +1,22 @@
+import { assertEquals } from "https://deno.land/std@0.203.0/assert/assert_equals.ts";
+import { randomInt } from "node:crypto";
+import { bigintRootApproximate } from "./bigint_root_approximate.ts";
+Deno.test("True Matrix 1", { permissions: "none" }, async (t) => {
+	const set = new Set();
+	for (let times = 0; times < 10000; times += 1) {
+		const radicandNumber = randomInt(0, 100000000);
+		const indexNumber = randomInt(0, 100000000);
+		const token = `${radicandNumber}//${indexNumber}`;
+		if (set.has(token)) {
+			times -= 1;
+			continue;
+		}
+		set.add(token);
+		await t.step(`Radicand: ${radicandNumber}; Index: ${indexNumber}`, () => {
+			const rootNumber = Math.pow(radicandNumber, 1 / indexNumber);
+			const rootBigInt = bigintRootApproximate(BigInt(radicandNumber), BigInt(indexNumber));
+			assertEquals(BigInt(Math.ceil(rootNumber)), rootBigInt.ceil);
+			assertEquals(BigInt(Math.floor(rootNumber)), rootBigInt.floor);
+		});
+	}
+});

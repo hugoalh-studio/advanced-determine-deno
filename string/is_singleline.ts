4196e85d9ec01eebb5cc6a300228bde156e0f587
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-11-04 17:33:03 +0800

    Improve function `isStringSingleLine`

--- string/is_singleline.ts
+++ string/is_singleline.ts
@@ -1,10 +1,10 @@
-const newLineRegExp = /[\n\r]/u;
+const regexpSingleLine = /^.+$/u;
 /**
  * Determine whether the string is single line.
  * @param {string} item Item that need to determine.
  * @returns {boolean} Determine result.
  */
 export function isStringSingleLine(item: string): boolean {
-	return !newLineRegExp.test(item);
+	return regexpSingleLine.test(item);
 }
 export default isStringSingleLine;

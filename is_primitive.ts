ec168109fa77d1bff6bf7265830f3a387d0d360e
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-11-07 16:41:15 +0800

    Fix missing JSDoc

--- is_primitive.ts
+++ is_primitive.ts
@@ -2,6 +2,11 @@
  * Type of JavaScript primitive.
  */
 export type Primitive = bigint | boolean | null | number | string | symbol | undefined;
+/**
+ * Determine whether the item is a primitive.
+ * @param {unknown} item Item that need to determine.
+ * @returns {item is Primitive} Determine result.
+ */
 export function isPrimitive(item: unknown): item is Primitive {
 	switch (typeof item) {
 		case "bigint":

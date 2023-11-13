9fa97c91780a227c160af1af804fba041d306d6f
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-11-13 18:00:09 +0800

    Add `isEnvironmentPodman`

---
+++ environment/is_podman.ts
@@ -0,0 +1,33 @@
+/**
+ * Determine whether this process is inside Podman.
+ * 
+ * **Require Permission:**
+ * - **File System - Read (`allow-read`):**
+ *   - `/run/.containerenv`
+ * @returns {Promise<boolean>} Determine result.
+ */
+export async function isEnvironmentPodman(): Promise<boolean> {
+	try {
+		await Deno.stat("/run/.containerenv");
+		return true;
+	} catch {
+		return false;
+	}
+}
+export default isEnvironmentPodman;
+/**
+ * Determine whether this process is inside WSL (Windows Subsystem for Linux).
+ * 
+ * **Require Permission:**
+ * - **File System - Read (`allow-read`):**
+ *   - `/run/.containerenv`
+ * @returns {boolean} Determine result.
+ */
+export function isEnvironmentPodmanSync(): boolean {
+	try {
+		Deno.statSync("/run/.containerenv");
+		return true;
+	} catch {
+		return false;
+	}
+}

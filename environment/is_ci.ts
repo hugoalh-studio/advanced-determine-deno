ce3cbefe96027ec7b4f6f99dd10f8fd5b2a47b10
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-11-14 11:21:02 +0800

    Fix JSDoc and matcher

--- environment/is_ci.ts
+++ environment/is_ci.ts
@@ -2,14 +2,13 @@
  * Determine whether this process is in CI (Continuous Integration) mode.
  * 
  * **Require Permission:**
- * - **Environment (`allow-env`):**
- *   - *All*
+ * - **Environment (`allow-env`):** *All*
  * @returns {boolean} Determine result.
  */
 export function isEnvironmentCI(): boolean {
 	const envCI: string | undefined = Deno.env.get("CI");
 	return (
-		(typeof envCI !== "undefined" && envCI !== "false" && envCI !== "0") ||
+		(typeof envCI !== "undefined" && !/^false$/iu.test(envCI) && envCI !== "0") ||
 		typeof Deno.env.get("CONTINUOUS_INTEGRATION") !== "undefined" ||
 		Object.keys(Deno.env.toObject()).some((key: string): boolean => {
 			return key.startsWith("CI_");

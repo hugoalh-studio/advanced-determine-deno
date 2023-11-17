26aba844155ddf349e9f76f6a96edbc0229b6d33
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-11-17 11:09:12 +0800

    Add function `isEnvironmentWSL` (#2)

---
+++ environment/is_wsl.ts
@@ -0,0 +1,46 @@
+/**
+ * @access private
+ * @param {Uint8Array} data
+ * @returns {boolean}
+ */
+function isEnvironmentWSLDataMatcher(data: Uint8Array): boolean {
+	const dataString: string = new TextDecoder("utf-8").decode(data);
+	return /microsoft/iu.test(dataString);
+}
+/**
+ * Determine whether this process is inside WSL (Windows Subsystem for Linux).
+ * 
+ * **Require Permission:**
+ * - **File System - Read (`allow-read`):**
+ *   - `/proc/version`
+ * @returns {Promise<boolean>} Determine result.
+ */
+export async function isEnvironmentWSL(): Promise<boolean> {
+	if (Deno.build.os !== "linux") {
+		return false;
+	}
+	try {
+		return isEnvironmentWSLDataMatcher(await Deno.readFile("/proc/version"));
+	} catch {
+		return false;
+	}
+}
+export default isEnvironmentWSL;
+/**
+ * Determine whether this process is inside WSL (Windows Subsystem for Linux).
+ * 
+ * **Require Permission:**
+ * - **File System - Read (`allow-read`):**
+ *   - `/proc/version`
+ * @returns {boolean} Determine result.
+ */
+export function isEnvironmentWSLSync(): boolean {
+	if (Deno.build.os !== "linux") {
+		return false;
+	}
+	try {
+		return isEnvironmentWSLDataMatcher(Deno.readFileSync("/proc/version"));
+	} catch {
+		return false;
+	}
+}

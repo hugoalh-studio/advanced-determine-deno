96b1b3f8994c4ab621f9a7d188b9abbab6b2bb36
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-11-17 11:09:43 +0800

    Add function `isEnvironmentDocker` (#4)

---
+++ environment/is_docker.ts
@@ -0,0 +1,54 @@
+/**
+ * @access private
+ * @param {Uint8Array} data
+ * @returns {boolean}
+ */
+function isEnvironmentDockerDataMatcher(data: Uint8Array): boolean {
+	const dataString: string = new TextDecoder("utf-8").decode(data);
+	return /docker/iu.test(dataString);
+}
+/**
+ * Determine whether this process is inside Docker.
+ * 
+ * **Require Permission:**
+ * - **File System - Read (`allow-read`):**
+ *   - `/.dockerenv`
+ *   - `/proc/self/cgroup`
+ * @returns {Promise<boolean>} Determine result.
+ */
+export async function isEnvironmentDocker(): Promise<boolean> {
+	try {
+		await Deno.stat("/.dockerenv");
+		return true;
+	} catch {
+		// Continue on error.
+	}
+	try {
+		return isEnvironmentDockerDataMatcher(await Deno.readFile("/proc/self/cgroup"));
+	} catch {
+		return false;
+	}
+}
+export default isEnvironmentDocker;
+/**
+ * Determine whether this process is inside Docker.
+ * 
+ * **Require Permission:**
+ * - **File System - Read (`allow-read`):**
+ *   - `/.dockerenv`
+ *   - `/proc/self/cgroup`
+ * @returns {boolean} Determine result.
+ */
+export function isEnvironmentDockerSync(): boolean {
+	try {
+		Deno.statSync("/.dockerenv");
+		return true;
+	} catch {
+		// Continue on error.
+	}
+	try {
+		return isEnvironmentDockerDataMatcher(Deno.readFileSync("/proc/self/cgroup"));
+	} catch {
+		return false;
+	}
+}

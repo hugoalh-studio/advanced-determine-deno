151aa3070c444c1d346f784e78dbc8675214fd06
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-11-30 17:06:41 +0800

    Fix test permissions

--- network/is_dns_clean.test.ts
+++ network/is_dns_clean.test.ts
@@ -1,14 +1,30 @@
 import { assertEquals } from "https://deno.land/std@0.208.0/assert/assert_equals.ts";
 import { isDNSClean } from "./is_dns_clean.ts";
-Deno.test("github.com - A", { permissions: "none" }, async () => {
+Deno.test("github.com - A", {
+	permissions: {
+		net: true
+	}
+}, async () => {
 	assertEquals(await isDNSClean("github.com", "A"), true);
 });
-Deno.test("github.com - AAAA", { permissions: "none" }, async () => {
+Deno.test("github.com - AAAA", {
+	permissions: {
+		net: true
+	}
+}, async () => {
 	assertEquals(await isDNSClean("github.com", "AAAA"), true);
 });
-Deno.test("google.com - A", { permissions: "none" }, async () => {
+Deno.test("google.com - A", {
+	permissions: {
+		net: true
+	}
+}, async () => {
 	assertEquals(await isDNSClean("google.com", "A"), true);
 });
-Deno.test("google.com - AAAA", { permissions: "none" }, async () => {
+Deno.test("google.com - AAAA", {
+	permissions: {
+		net: true
+	}
+}, async () => {
 	assertEquals(await isDNSClean("google.com", "AAAA"), true);
 });

9dee1f3b51d24d1efeb0e362c8329d79a64549cb
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-11-30 17:23:16 +0800

    Update test

--- network/is_dns_clean.test.ts
+++ network/is_dns_clean.test.ts
@@ -1,6 +1,16 @@
 import { assertEquals } from "https://deno.land/std@0.208.0/assert/assert_equals.ts";
+import regexpIP from "https://esm.sh/ip-regex@^5.0.0";
 import { isDNSClean } from "./is_dns_clean.ts";
+const myIP = await fetch("https://api64.ipify.org/", {
+	method: "GET",
+	redirect: "error"
+}).then((response) => {
+	return response.text();
+});
+const isMyIPv4 = regexpIP.v4({ exact: true }).test(myIP);
+const isMyIPv6 = regexpIP.v6({ exact: true }).test(myIP);
 Deno.test("github.com - A", {
+	ignore: !isMyIPv4,
 	permissions: {
 		net: true
 	}
@@ -8,6 +18,7 @@ Deno.test("github.com - A", {
 	assertEquals(await isDNSClean("github.com", "A"), true);
 });
 Deno.test("github.com - AAAA", {
+	ignore: !isMyIPv6,
 	permissions: {
 		net: true
 	}
@@ -15,6 +26,7 @@ Deno.test("github.com - AAAA", {
 	assertEquals(await isDNSClean("github.com", "AAAA"), true);
 });
 Deno.test("google.com - A", {
+	ignore: !isMyIPv4,
 	permissions: {
 		net: true
 	}
@@ -22,9 +34,26 @@ Deno.test("google.com - A", {
 	assertEquals(await isDNSClean("google.com", "A"), true);
 });
 Deno.test("google.com - AAAA", {
+	ignore: !isMyIPv6,
 	permissions: {
 		net: true
 	}
 }, async () => {
 	assertEquals(await isDNSClean("google.com", "AAAA"), true);
+});
+Deno.test("microsoft.com - A", {
+	ignore: !isMyIPv4,
+	permissions: {
+		net: true
+	}
+}, async () => {
+	assertEquals(await isDNSClean("microsoft.com", "A"), true);
+});
+Deno.test("microsoft.com - AAAA", {
+	ignore: !isMyIPv6,
+	permissions: {
+		net: true
+	}
+}, async () => {
+	assertEquals(await isDNSClean("microsoft.com", "AAAA"), true);
 });

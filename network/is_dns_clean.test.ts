d00944bcd60c609dc8456212be8e5d250d74b5f5
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-12-01 13:13:12 +0800

    Update test

--- network/is_dns_clean.test.ts
+++ network/is_dns_clean.test.ts
@@ -1,15 +1,15 @@
 import { isDNSClean } from "./is_dns_clean.ts";
-import { isIPValid } from "./is_ip_valid.ts";
-const myIP = await fetch("https://api64.ipify.org/", {
+import { resolveIPVersion } from "./ip_version.ts";
+const myIPVersion = await fetch("https://api64.ipify.org/", {
 	method: "GET",
 	redirect: "error"
 }).then((response) => {
 	return response.text();
+}).then((ip) => {
+	return resolveIPVersion(ip);
 });
-const isMyIPv4 = isIPValid(myIP, 4);
-const isMyIPv6 = isIPValid(myIP, 6);
 Deno.test("github.com - A", {
-	ignore: !isMyIPv4,
+	ignore: myIPVersion !== 4,
 	permissions: {
 		net: true
 	}
@@ -17,7 +17,7 @@ Deno.test("github.com - A", {
 	void await isDNSClean("github.com", "A");
 });
 Deno.test("github.com - AAAA", {
-	ignore: !isMyIPv6,
+	ignore: myIPVersion !== 6,
 	permissions: {
 		net: true
 	}
@@ -25,7 +25,7 @@ Deno.test("github.com - AAAA", {
 	void await isDNSClean("github.com", "AAAA");
 });
 Deno.test("google.com - A", {
-	ignore: !isMyIPv4,
+	ignore: myIPVersion !== 4,
 	permissions: {
 		net: true
 	}
@@ -33,7 +33,7 @@ Deno.test("google.com - A", {
 	void await isDNSClean("google.com", "A");
 });
 Deno.test("google.com - AAAA", {
-	ignore: !isMyIPv6,
+	ignore: myIPVersion !== 6,
 	permissions: {
 		net: true
 	}
@@ -41,7 +41,7 @@ Deno.test("google.com - AAAA", {
 	void await isDNSClean("google.com", "AAAA");
 });
 Deno.test("microsoft.com - A", {
-	ignore: !isMyIPv4,
+	ignore: myIPVersion !== 4,
 	permissions: {
 		net: true
 	}
@@ -49,7 +49,7 @@ Deno.test("microsoft.com - A", {
 	void await isDNSClean("microsoft.com", "A");
 });
 Deno.test("microsoft.com - AAAA", {
-	ignore: !isMyIPv6,
+	ignore: myIPVersion !== 6,
 	permissions: {
 		net: true
 	}

4b115dfb0b2c72bfd02b580bbe9766a6ebcc7653
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-12-01 12:57:04 +0800

    Update test

--- network/is_dns_clean.test.ts
+++ network/is_dns_clean.test.ts
@@ -1,13 +1,13 @@
-import regexpIP from "https://esm.sh/ip-regex@^5.0.0";
 import { isDNSClean } from "./is_dns_clean.ts";
+import { isIPValid } from "./is_ip_valid.ts";
 const myIP = await fetch("https://api64.ipify.org/", {
 	method: "GET",
 	redirect: "error"
 }).then((response) => {
 	return response.text();
 });
-const isMyIPv4 = regexpIP.v4({ exact: true }).test(myIP);
-const isMyIPv6 = regexpIP.v6({ exact: true }).test(myIP);
+const isMyIPv4 = isIPValid(myIP, 4);
+const isMyIPv6 = isIPValid(myIP, 6);
 Deno.test("github.com - A", {
 	ignore: !isMyIPv4,
 	permissions: {

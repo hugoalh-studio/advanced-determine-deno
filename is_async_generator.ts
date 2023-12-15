30d5fd1d7b1e7a2fa740e65043da1448464b41c8
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-12-15 11:42:50 +0800

    Fix type asserts

--- is_async_generator.ts
+++ is_async_generator.ts
@@ -4,9 +4,9 @@ import { types } from "node:util";
  * 
  * This only reports back what the JavaScript engine is seeing; In particular, the return value may not match the original source code if a transpilation tool was used.
  * @param {unknown} item Item that need to determine.
- * @returns {item is AsyncGenerator<unknown, unknown, unknown>} Determine result.
+ * @returns {item is AsyncGenerator} Determine result.
  */
-export function isAsyncGenerator(item: unknown): item is AsyncGenerator<unknown, unknown, unknown> {
+export function isAsyncGenerator(item: unknown): item is AsyncGenerator {
 	return (types.isGeneratorObject(item) && Object.prototype.toString.call(item) === "[object AsyncGenerator]");
 }
 export {
--- is_sync_generator.ts
+++ is_sync_generator.ts
@@ -4,9 +4,9 @@ import { types } from "node:util";
  * 
  * This only reports back what the JavaScript engine is seeing; In particular, the return value may not match the original source code if a transpilation tool was used.
  * @param {unknown} item Item that need to determine.
- * @returns {item is Generator<unknown, unknown, unknown>} Determine result.
+ * @returns {item is Generator} Determine result.
  */
-export function isSyncGenerator(item: unknown): item is Generator<unknown, unknown, unknown> {
+export function isSyncGenerator(item: unknown): item is Generator {
 	return (types.isGeneratorObject(item) && Object.prototype.toString.call(item) === "[object Generator]");
 }
 export {

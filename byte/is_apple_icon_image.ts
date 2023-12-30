d368313679d2dc49c4b4d996dc7f0d1a7a5cb4a5
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-12-30 17:25:45 +0800

    Add function `isByteAppleIconImage`

---
+++ byte/is_apple_icon_image.ts
@@ -0,0 +1,10 @@
+import { isByteMatch } from "./_matcher.ts";
+/**
+ * Determine whether the item is Apple Icon Image (`.icns`) file format.
+ * @param {Uint8Array} item Item that need to determine.
+ * @returns {boolean} Determine result.
+ */
+export function isByteAppleIconImage(item: Uint8Array): boolean {
+	return isByteMatch(item, "69 63 6e 73");
+}
+export default isByteAppleIconImage;

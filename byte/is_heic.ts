feacb899327831a2f84e03d51d036d41b15b8fba
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-12-30 17:25:52 +0800

    Add function `isByteHEIC`

---
+++ byte/is_heic.ts
@@ -0,0 +1,10 @@
+import { isByteMatch } from "./_matcher.ts";
+/**
+ * Determine whether the item is HEIC (High Efficiency Image Container) (`.heic`) file format.
+ * @param {Uint8Array} item Item that need to determine.
+ * @returns {boolean} Determine result.
+ */
+export function isByteHEIC(item: Uint8Array): boolean {
+	return isByteMatch(item, "66 74 79 70 68 65 69 63", 4);
+}
+export default isByteHEIC;

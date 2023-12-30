ca5e69086c99138158b414bdf1ae027a2e0b5c71
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-12-30 13:51:20 +0800

    Add function `isByteEPub`

---
+++ byte/is_epub.ts
@@ -0,0 +1,10 @@
+import { isByteMatch } from "./_matcher.ts";
+/**
+ * Determine whether the item is ePUB (`.epub`) file format.
+ * @param {Uint8Array} item Item that need to determine.
+ * @returns {boolean} Determine result.
+ */
+export function isByteEPub(item: Uint8Array): boolean {
+	return (isByteMatch(item, ["50 4B 03 04"]) && item.slice(30, 58).toString() === "mimetypeapplication/epub+zip");
+}
+export default isByteEPub;

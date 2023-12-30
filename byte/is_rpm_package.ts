77fd2d162a858dc16fc4e3ad30084493a4237c15
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-12-30 13:54:11 +0800

    Add function `isByteRpmPackage`

---
+++ byte/is_rpm_package.ts
@@ -0,0 +1,10 @@
+import { isByteMatch } from "./_matcher.ts";
+/**
+ * Determine whether the item is RedHat Package Manager (RPM) package (`.rpm`) file format.
+ * @param {Uint8Array} item Item that need to determine.
+ * @returns {boolean} Determine result.
+ */
+export function isByteRpmPackage(item: Uint8Array): boolean {
+	return isByteMatch(item, ["ED AB EE DB"]);
+}
+export default isByteRpmPackage;

971825e4fb5eca52084543f2ee53427ef51090e9
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-12-30 13:53:04 +0800

    Add function `isBytePcapNG`

---
+++ byte/is_pcapng.ts
@@ -0,0 +1,10 @@
+import { isByteMatch } from "./_matcher.ts";
+/**
+ * Determine whether the item is PCAP Next Generation Dump (`.pcapng`) file format.
+ * @param {Uint8Array} item Item that need to determine.
+ * @returns {boolean} Determine result.
+ */
+export function isBytePcapNG(item: Uint8Array): boolean {
+	return isByteMatch(item, ["0A 0D 0D 0A"]);
+}
+export default isBytePcapNG;

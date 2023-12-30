197dc6916d957bd45eef3feeab290ce2c13bed4e
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-12-30 13:55:54 +0800

    Add function `isByteWebP`

---
+++ byte/is_webp.ts
@@ -0,0 +1,10 @@
+import { isByteMatch } from "./_matcher.ts";
+/**
+ * Determine whether the item is WebP (`.webp`) file format.
+ * @param {Uint8Array} item Item that need to determine.
+ * @returns {boolean} Determine result.
+ */
+export function isByteWebP(item: Uint8Array): boolean {
+	return (isByteMatch(item, ["52 49 46 46"]) && isByteMatch(item, ["57 45 42 50"], 8));
+}
+export default isByteWebP;

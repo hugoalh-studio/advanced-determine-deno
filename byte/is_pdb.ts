e58ee21e87c984b4731e6657485a6576e0e9357a
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-12-30 13:53:18 +0800

    Add function `isBytePdb`

---
+++ byte/is_pdb.ts
@@ -0,0 +1,10 @@
+import { isByteMatch } from "./_matcher.ts";
+/**
+ * Determine whether the item is PalmPilot Database/Document (`.pdb`) file format.
+ * @param {Uint8Array} item Item that need to determine.
+ * @returns {boolean} Determine result.
+ */
+export function isBytePdb(item: Uint8Array): boolean {
+	return isByteMatch(item, ["00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"], 11);
+}
+export default isBytePdb;

c0540ff294b549f15cecb82e4614301393f5e1a0
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-12-30 17:51:25 +0800

    Add function `isByteAmiBackAmigaBackupDataFile`

---
+++ byte/is_amiback_amiga_backup_data_file.ts
@@ -0,0 +1,10 @@
+import { isByteMatch } from "./_matcher.ts";
+/**
+ * Determine whether the byte is AmiBack Amiga Backup data file (`.bac`) format.
+ * @param {Uint8Array} item Item that need to determine.
+ * @returns {boolean} Determine result.
+ */
+export function isByteAmiBackAmigaBackupDataFile(item: Uint8Array): boolean {
+	return isByteMatch(item, "42 41 43 4B 4D 49 4B 45 44 49 53 4B");
+}
+export default isByteAmiBackAmigaBackupDataFile;

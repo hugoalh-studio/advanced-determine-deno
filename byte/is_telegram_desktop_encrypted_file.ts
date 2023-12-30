e2efd208f62a4a6f96c3dd6f84517dc6f0cc090b
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-12-30 17:26:33 +0800

    Add function `isByteTelegramDesktopEncryptedFile`

---
+++ byte/is_telegram_desktop_encrypted_file.ts
@@ -0,0 +1,10 @@
+import { isByteMatch } from "./_matcher.ts";
+/**
+ * Determine whether the item is Telegram Desktop Encrypted (`.tdef`) file format.
+ * @param {Uint8Array} item Item that need to determine.
+ * @returns {boolean} Determine result.
+ */
+export function isByteTelegramDesktopEncryptedFile(item: Uint8Array): boolean {
+	return isByteMatch(item, "54 44 45 46");
+}
+export default isByteTelegramDesktopEncryptedFile;

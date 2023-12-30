9a3e1e45457355c17c961e4af0c0eb913cfd5cff
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-12-30 13:52:52 +0800

    Add function `isBytePcap`

---
+++ byte/is_pcap.ts
@@ -0,0 +1,15 @@
+import { isByteMatch } from "./_matcher.ts";
+/**
+ * Determine whether the item is Libpcap (`.pcap`) file format.
+ * @param {Uint8Array} item Item that need to determine.
+ * @returns {boolean} Determine result.
+ */
+export function isBytePcap(item: Uint8Array): boolean {
+	return isByteMatch(item, [
+		"D4 C3 B2 A1",
+		"A1 B2 C3 D4",
+		"4D 3C B2 A1",
+		"A1 B2 3C 4D"
+	]);
+}
+export default isBytePcap;

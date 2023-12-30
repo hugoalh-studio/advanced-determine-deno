8d19a645d0340423db48bd35f0f1ee968a40246e
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-12-30 17:25:58 +0800

    Add function `isByteICO`

---
+++ byte/is_ico.ts
@@ -0,0 +1,10 @@
+import { isByteMatch } from "./_matcher.ts";
+/**
+ * Determine whether the item is ICO (`.ico`) file format.
+ * @param {Uint8Array} item Item that need to determine.
+ * @returns {boolean} Determine result.
+ */
+export function isByteICO(item: Uint8Array): boolean {
+	return isByteMatch(item, "00 00 01 00");
+}
+export default isByteICO;

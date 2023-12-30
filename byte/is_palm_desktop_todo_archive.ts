c33b131ca937f11425b0f1818d66f45f5a8a153e
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-12-30 17:26:21 +0800

    Add function `isBytePalmDesktopToDoArchive`

---
+++ byte/is_palm_desktop_todo_archive.ts
@@ -0,0 +1,10 @@
+import { isByteMatch } from "./_matcher.ts";
+/**
+ * Determine whether the item is Palm Desktop To Do Archive (`.dba`) file format.
+ * @param {Uint8Array} item Item that need to determine.
+ * @returns {boolean} Determine result.
+ */
+export function isBytePalmDesktopToDoArchive(item: Uint8Array): boolean {
+	return isByteMatch(item, "00 01 42 44");
+}
+export default isBytePalmDesktopToDoArchive;

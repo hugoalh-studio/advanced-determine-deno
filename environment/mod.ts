549915185d71848edba59767e9afa3ea4a8d1be5
hugoalh <32359235+hugoalh@users.noreply.github.com>
2024-01-11 16:37:20 +0800

    Update mod.ts

--- environment/mod.ts
+++ environment/mod.ts
@@ -1,7 +1,11 @@
 export { isEnvironmentCI } from "./is_ci.ts";
 export { isEnvironmentDocker, isEnvironmentDockerSync } from "./is_docker.ts";
+export { isEnvironmentHeroku } from "./is_heroku.ts";
+export { isEnvironmentHyper } from "./is_hyper.ts";
 export { isEnvironmentPodman, isEnvironmentPodmanSync } from "./is_podman.ts";
+export { isEnvironmentRoot } from "./is_root.ts";
 export { isEnvironmentSSH } from "./is_ssh.ts";
+export { isEnvironmentTravis } from "./is_travis.ts";
 export { isEnvironmentWSL, isEnvironmentWSLSync } from "./is_wsl.ts";
 export { getEnvironmentPaths } from "./path.ts";
 export { getEnvironmentPathExts } from "./pathext.ts";

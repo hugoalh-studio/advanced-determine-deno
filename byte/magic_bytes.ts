fd530e5242302049281790c82646dc51f6b7b49e
hugoalh <32359235+hugoalh@users.noreply.github.com>
2024-02-19 12:14:53 +0800

    Improve bytes matcher

--- byte/magic_bytes.ts
+++ byte/magic_bytes.ts
@@ -21,90 +21,88 @@ export interface MagicBytesMeta {
 	 */
 	patternVariant?: string;
 }
-interface MagicBytesEntry<T extends string | Uint8Array> extends MagicBytesMeta {
-	pattern: BytesMatcherPattern<T>[];
+interface MagicBytesEntry extends MagicBytesMeta {
+	pattern: BytesMatcherPattern<string>[];
 }
 export interface MagicBytesMetaWithWeight extends MagicBytesMeta {
 	/**
 	 * Weight of the magic bytes.
 	 */
-	weight: bigint;
+	weight: number;
 }
 interface MagicBytesListEntry {
 	matcher: BytesMatcher;
-	meta: MagicBytesMeta;
-	weight: bigint;
+	meta: MagicBytesMetaWithWeight;
 }
 /**
  * Magic bytes matcher to determine whether the bytes is match the specify magic bytes.
  */
 export class MagicBytesMatcher {
-	#list: Set<MagicBytesListEntry> = new Set<MagicBytesListEntry>();
+	#bytesMinimum = 0;
+	#list: MagicBytesListEntry[] = [];
 	/**
 	 * Initialize magic bytes matcher.
 	 * @param {(meta: MagicBytesMeta) => boolean} [filter] Filter.
 	 */
 	constructor(filter?: (meta: MagicBytesMeta) => boolean) {
-		for (const { pattern, ...meta } of (MagicBytesList as unknown as MagicBytesEntry<string>[])) {
+		for (const { pattern, ...meta } of (MagicBytesList as unknown as MagicBytesEntry[])) {
 			if (filter?.(meta) ?? true) {
 				const matcher: BytesMatcher = new BytesMatcher(pattern);
-				this.#list.add({
+				this.#list.push({
 					matcher,
-					meta,
-					weight: matcher.weight
+					meta: {
+						...meta,
+						weight: matcher.weight
+					}
 				});
+				this.#bytesMinimum = Math.max(this.#bytesMinimum, matcher.bytesMinimum);
 			}
 		}
-		if (this.#list.size === 0) {
+		if (Object.entries(this.#list).length === 0) {
 			throw new Error(`Matcher is empty!`);
 		}
+		this.#list.sort((a: MagicBytesListEntry, b: MagicBytesListEntry): number => {
+			return b.meta.weight - a.meta.weight;
+		});
 	}
 	/**
 	 * List all of the magic bytes meta which the bytes is match.
-	 * @param item Item that need to determine.
+	 * @param {string | Uint8Array} item Item that need to determine.
 	 * @returns {Generator<MagicBytesMetaWithWeight>} Magic bytes meta list.
 	 */
-	*matchAllIterate(item: Uint8Array): Generator<MagicBytesMetaWithWeight> {
-		for (const { matcher, meta, weight } of this.#list.values()) {
+	*matchAllIterate(item: string | Uint8Array): Generator<MagicBytesMetaWithWeight> {
+		for (const { matcher, meta } of this.#list.values()) {
 			if (matcher.test(item)) {
-				yield { ...meta, weight };
+				yield meta;
 			}
 		}
 	}
 	/**
 	 * List all of the magic bytes meta which the bytes is match.
-	 * @param item Item that need to determine.
+	 * @param {string | Uint8Array} item Item that need to determine.
 	 * @returns {MagicBytesMetaWithWeight[]} Magic bytes meta list.
 	 */
-	matchAll(item: Uint8Array): MagicBytesMetaWithWeight[] {
+	matchAll(item: string | Uint8Array): MagicBytesMetaWithWeight[] {
 		return Array.from(this.matchAllIterate(item));
 	}
 	/**
 	 * Return the magic bytes meta which the bytes is match the closest.
-	 * @param item Item that need to determine.
+	 * @param {string | Uint8Array} item Item that need to determine.
 	 * @returns {MagicBytesMetaWithWeight | null} Magic bytes meta.
 	 */
-	match(item: Uint8Array): MagicBytesMetaWithWeight | null {
-		return this.matchAll(item).sort((a: MagicBytesMetaWithWeight, b: MagicBytesMetaWithWeight): number => {
-			if (a.weight > b.weight) {
-				return 1;
-			}
-			if (a.weight < b.weight) {
-				return -1;
-			}
-			return 0;
-		})[0] ?? null;
+	match(item: string | Uint8Array): MagicBytesMetaWithWeight | null {
+		for (const _ of this.matchAllIterate(item)) {
+			return _;
+		}
+		return null;
 	}
 	/**
 	 * Determine whether the bytes is match any of specify magic bytes.
-	 * @param {Uint8Array} item Item that need to determine.
+	 * @param {string | Uint8Array} item Item that need to determine.
 	 * @returns {boolean} Determine result.
 	 */
-	test(item: Uint8Array): boolean {
-		for (const _ of this.matchAllIterate(item)) {
-			return true;
-		}
-		return false;
+	test(item: string | Uint8Array): boolean {
+		return (this.match(item) !== null);
 	}
 }
 export default MagicBytesMatcher;
--- byte/matcher.ts
+++ byte/matcher.ts
@@ -1,15 +1,14 @@
-import { includesNeedle as bytesIncludesNeedle } from "https://deno.land/std@0.216.0/bytes/includes_needle.ts";
 const regexpPatternHex = /^[\dA-F]{2}(?: [\dA-F]{2})*$/v;
 export interface BytesMatcherPattern<T extends string | Uint8Array> {
-	fromIndex: "*" | number;
+	fromIndex: number;
 	hex: T;
 }
 /**
  * Bytes matcher to determine whether the bytes is match the specify pattern.
  */
 export class BytesMatcher {
-	#pattern: BytesMatcherPattern<Uint8Array>[];
-	#weight: bigint;
+	#bytesMinimum: number;
+	#pattern: Map<number, number> = new Map<number, number>();
 	/**
 	 * Initialize bytes matcher.
 	 * @param {BytesMatcherPattern<string | Uint8Array>[]} pattern Pattern.
@@ -18,56 +17,69 @@ export class BytesMatcher {
 		if (pattern.length === 0) {
 			throw new TypeError(`Argument \`pattern\` is not defined!`);
 		}
-		this.#pattern = pattern.map(({ fromIndex, hex }: BytesMatcherPattern<string | Uint8Array>): BytesMatcherPattern<Uint8Array> => {
-			if (fromIndex !== "*" && !Number.isSafeInteger(fromIndex)) {
-				throw new SyntaxError(`\`${fromIndex}\` is not a valid index!`);
+		for (const { fromIndex: indexFrom, hex } of pattern) {
+			if (!Number.isSafeInteger(indexFrom)) {
+				throw new SyntaxError(`\`${indexFrom}\` is not a valid index!`);
 			}
-			if (hex instanceof Uint8Array) {
-				return {
-					fromIndex,
-					hex: Uint8Array.of(...hex)
-				};
+			if (hex.length === 0) {
+				throw new SyntaxError(`Hex is empty from index ${indexFrom}!`);
 			}
-			if (!regexpPatternHex.test(hex)) {
+			if (typeof hex === "string" && !regexpPatternHex.test(hex)) {
 				throw new SyntaxError(`\`${hex}\` is not a valid hex!`);
 			}
-			return {
-				fromIndex,
-				hex: Uint8Array.of(...hex.split(" ").map((byte: string): number => {
-					return Number.parseInt(byte, 16);
-				}))
-			};
-		});
-		this.#weight = this.#pattern.map(({ hex }: BytesMatcherPattern<Uint8Array>): bigint => {
-			return BigInt(hex.length);
-		}).reduce((valuePrevious: bigint, valueCurrent: bigint): bigint => {
-			return (valuePrevious + valueCurrent);
-		}, 0n);
+			const hexResolve: number[] = Array.from((typeof hex === "string") ? Uint8Array.of(...hex.split(" ").map((byte: string): number => {
+				return Number.parseInt(byte, 16);
+			})) : Uint8Array.of(...hex));
+			const indexTo: number = indexFrom + hexResolve.length;
+			if (indexFrom < 0 && indexTo > 0) {
+				throw new Error(`Trail of the pattern is overflow (most likely cause by incorrect index)! Current: ${indexFrom}; Expect: ${indexFrom - indexTo}`);
+			}
+			for (let indexCursor: number = indexFrom, indexHex = 0; indexCursor < indexTo; indexCursor += 1, indexHex += 1) {
+				const byte: number = hexResolve[indexHex];
+				if (this.#pattern.has(indexCursor)) {
+					throw new SyntaxError(`Index of ${indexCursor} is already defined! Exist: ${this.#pattern.get(indexCursor)!.toString(16)}; Override: ${byte.toString(16)}`);
+				}
+				this.#pattern.set(indexCursor, byte);
+			}
+		}
+		if (this.#pattern.size === 0) {
+			throw new Error(`Pattern is empty!`);
+		}
+		const indexes: number[] = Array.from(this.#pattern.keys());
+		this.#bytesMinimum = indexes.some((index: number): boolean => {
+			return (index < 0);
+		}) ? Infinity : Math.max(...indexes);
 	}
 	/**
 	 * Determine whether the bytes is match the specify pattern.
-	 * @param {Uint8Array} item Item that need to determine.
+	 * @param {string | Uint8Array} item Item that need to determine.
 	 * @returns {boolean} Determine result.
 	 */
-	test(item: Uint8Array): boolean {
-		return this.#pattern.every(({ fromIndex, hex }: BytesMatcherPattern<Uint8Array>): boolean => {
-			if (fromIndex === "*") {
-				return bytesIncludesNeedle(item, hex);
-			}
-			const itemSlice: Uint8Array = (fromIndex >= 0) ? item.slice(fromIndex, fromIndex + hex.length) : item.slice(item.length + fromIndex, item.length + fromIndex + hex.length);
-			if (itemSlice.length !== hex.length) {
+	test(item: string | Uint8Array): boolean {
+		if (item.length === 0) {
+			return false;
+		}
+		const itemResolve: Uint8Array = (typeof item === "string") ? new TextEncoder().encode(item) : Uint8Array.of(...item);
+		for (const [index, byte] of this.#pattern.entries()) {
+			if (itemResolve[index] !== byte) {
 				return false;
 			}
-			return hex.every((byte: number, index: number): boolean => {
-				return (itemSlice[index] === byte);
-			});
-		});
+		}
+		return true;
 	}
 	/**
-	 * Weight of the bytes matcher.
+	 * Require minimum bytes to read in the stream for the bytes matcher. Useful for file stream.
+	 * @returns {number} Minimum bytes to read in the stream.
 	 */
-	get weight(): bigint {
-		return this.#weight;
+	get bytesMinimum(): number {
+		return this.#bytesMinimum;
+	}
+	/**
+	 * Weight of the bytes matcher. Useful for reduce percentage of false positive.
+	 * @returns {number} Weight of the bytes matcher.
+	 */
+	get weight(): number {
+		return this.#pattern.size;
 	}
 }
 export default BytesMatcher;
